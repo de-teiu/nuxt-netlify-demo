3c56d443c1e9ffcbb017da7e8b7b3486f9ac5f6b
LAPTOP-SGEIJU4B\tekur <tekuragari@gmail.com>
2018-11-07 21:32:49 +0900

    戻す

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <app-logo />
       <h1 class="title">
-        nuxt-netlify-demo-ゆるWeb
+        nuxt-netlify-demo
       </h1>
       <h2 class="subtitle">
         Nuxt.js project
