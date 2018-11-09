f26aa1b10225e7e323a02b8327fe9660767976fc
LAPTOP-SGEIJU4B\tekur <tekuragari@gmail.com>
2018-11-09 20:40:50 +0900

    デモ用

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,19 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo />
-      <h1 class="title">
-        nuxt-netlify-demo
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+    <section class="container">
+        <div>
+            <app-logo />
+            <h1 class="title">
+                nuxt-netlify-demoあああああ
+            </h1>
+            <h2 class="subtitle">
+                Nuxt.js project
+            </h2>
+            <div class="links">
+                <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+                <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
